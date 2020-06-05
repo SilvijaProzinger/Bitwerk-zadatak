7b278503d11e14c59b33052a79c7eb9cd69fdc68
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-06-05 17:01:20 +0200

    added validation check for input fields

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let inputFile = document.getElementById('fileInput')
 let form = document.getElementById('contactForm')
 let listOfFiles = [] 
+let submitButton = document.getElementById('submitButton')
 
 inputFile.addEventListener('change', function(event){
 	listOfFiles = []
@@ -12,10 +13,20 @@ inputFile.addEventListener('change', function(event){
 })
 
 form.addEventListener('submit', function(event){
-	//event.preventDefault()
-	listOfFiles.forEach(function(file){
-		sendFile(file)
-	})
+	let nameValue = document.forms['form']['name'].value;
+	let surnameValue = document.forms['form']['surname'].value;
+	let emailValue = document.forms['form']['email'].value
+
+	//check if any of the input fields is empty
+	if (nameValue === '' || surnameValue === '' || emailValue === ''){
+		alert('Please fill all required fields!')
+	} else {
+		//event.preventDefault()
+		listOfFiles.forEach(function(file){
+			sendFile(file)
+		})
+		alert('Thank you for your submission!')
+	}
 })
 
 //send the uploaded files with ajax 
@@ -28,3 +39,5 @@ sendFile = function(file){
 	request.open('POST', 'https://jsonplaceholder.typicode.com/posts')
 	request.send(formData)
 }
+
+ 
