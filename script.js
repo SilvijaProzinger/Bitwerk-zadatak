ccbaa00bfb0f5cc589f84786ff49b1b1a12e9e76
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-06-06 16:30:00 +0200

    added a blur to background behind modal

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputFile = document.getElementById('fileInput')
 let form = document.getElementById('contactForm')
 let listOfFiles = [] 
 let submitButton = document.getElementById('submitButton')
+let alertModal = document.getElementById('alert')
 
 inputFile.addEventListener('change', function(event){
 	listOfFiles = []
@@ -19,13 +20,28 @@ form.addEventListener('submit', function(event){
 
 	//check if any of the input fields is empty
 	if (nameValue === '' || surnameValue === '' || emailValue === '' || !listOfFiles.length){
-		alert('Please fill all required fields!')
+		alertModal.style.display = 'block'
+		document.getElementById('main').classList.add('modal-opened')
+		document.getElementById('modalText').innerHTML = 'Please fill out all required fields!'
+		document.getElementById('close').addEventListener('click', function(){
+			//on click close the alert modal window
+			alertModal.style.display = 'none'
+			document.getElementById('main').classList.remove('modal-opened')
+		})
 		event.preventDefault()
 	} else {
+		event.preventDefault()
+		alertModal.style.display = 'block'
+		document.getElementById('main').classList.add('modal-opened')
+		document.getElementById('modalText').innerHTML = 'Thank you for your submission!'
+		document.getElementById('close').addEventListener('click', function(){
+			alertModal.style.display = 'none'
+			document.getElementById('main').classList.remove('modal-opened')
+			form.submit()
+		})
 		listOfFiles.forEach(function(file){
 			sendFile(file)
 		})
-		alert('Thank you for your submission!')
 	}
 })
 
